2e152e28f153d05dc476dae821bfdba9f2afa57e
Evžen Wybitul <wybitul.evzen@gmail.com>
2019-05-07 15:59:20 +0200

    Add the beginning of the 2nd task solution

---
+++ doc/zaklady-bioinformatiky/tasks.html.pm
@@ -0,0 +1,20 @@
+#lang pollen
+
+◊define-meta[header]{tasks}
+◊define-meta[title]{Úkoly}
+
+◊title{Bonusové úkoly z bioinformatiky}
+
+Zde budou vypracované oba bonusové úkoly.
+
+◊section{Úkol #2}
+
+Vypracovaný podle ◊link["https://web.natur.cuni.cz/~marian/pract4.html"]{této stránky}.
+
+◊questions[2]{
+    ◊question{How would you qualify the protein based on the secondary structure content? All-alpha? All-beta? or mixed alpha-beta?}
+    ◊answer{Protein je alfa-beta.}
+
+    ◊question{Can you guess how many domains does the structure contain? Are they most probably identical or different?}
+    ◊answer{Podle obrázku nejspíše dvě stejné domény (pokud ty samotné neberu jako rozdělené na více domén).}
+}
