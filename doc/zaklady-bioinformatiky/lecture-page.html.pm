81723a69b22b615ff8dc2729823d982974600346
Evžen Wybitul <wybitul.evzen@gmail.com>
2019-05-09 14:21:22 +0200

    Add link to pdf version

--- doc/zaklady-bioinformatiky/lecture-page.html.pm
+++ doc/zaklady-bioinformatiky/lecture-page.html.pm
@@ -7,7 +7,7 @@
 ◊title{Základy bioinformatiky}
 
 ◊box["Zápisky (v1.0.0, poslední update 7. 5. 2019)"]{
-    K dispozici je odkaz na ◊link["notes.html"]{webovou verzi}.
+    K dispozici je odkaz na ◊link["notes.html"]{webovou verzi} a na ◊link["notes.pdf"]{dokument pdf}. PDF verze je WIP, prosím, hlašte chyby nebo nápady na vylepšení.
 
     Řešení bonusových úkolů jsou k dispozici ◊link["tasks.html"]{zde}.
 }
