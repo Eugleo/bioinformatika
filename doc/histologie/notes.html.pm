9a0d390c4c5ad792b1a319a686dee0fa90e13da7
Evžen Wybitul <wybitul.evzen@gmail.com>
2019-05-19 13:31:17 +0200

    Add notes about CNS

---
+++ doc/histologie/notes.html.pm
@@ -0,0 +1,237 @@
+#lang pollen
+
+◊define-meta[title]{Histologie}
+◊define-meta[header]{zápisy}
+
+◊title{Nervové buňky}
+
+◊ls{
+    - ontogeneticky i fylogeneticky odvozeny od epitelu
+        - některé z nich mají polarizovanou strukturu (ependymální gliové buňky mají řasinky)
+    - neurony, neuroepiteliální smyslové buňky, gliové buňky
+    - mnoho rozdílů mezi buňkami, patří zde nejmenší i největší buněčné typy
+}
+
+◊section{Schopnost regenerace}
+
+Naproti obecné představě jsou nervové buňky schopny určité regenerace.
+
+◊ls{
+    # Ve zdravém neuronu spojeném se svalem je jádro uprostřed a je v něm mnoho Nisslových tělísek.
+    # Když je axon přerušen, jádro se posune na periferii neuronu a počet Nisslových tělísek je velice sníží. Část nervového vlákna, která je nyní spojená jen se svalem, degeneruje a je odklizena makrofágy.
+    # Denervovaná svalová buňka atrofuje. Schwannovy buňky proliferují, tvoří silný kabel roustoucí ze svalové buňky.
+    # Axon dorůstá a snaží se spojit a prorůst Schannovými buňkami.
+        - Když se mu to povede, sval je opět inervovaný, obnoví se jeho síla i funkce a neuron se vrátí do původního stavu.
+        - Když se mu to nepovede, růst axonu je neorganizovaný, sval dál atrofuje. Po překročení určité doby je sval už nenávratně poškozen.
+}
+
+Axony málokdy najdou přesně tu správnou myelinovou pochvu a přesně to správné místo, kde původně vedly---jednotlivé svaly mají po regeneraci po zranění nejprve špatnou koordinaci a mozek se musí přeučovat, což trvá měsíce až roky.
+
+U myši jsou schopna se zahojit i poranění páteře; při poraněních páteře u člověka je ale problém s tím, že je informační zmatek přerušených axonů obrovský, navíc axony by musely prorůst mnohem dál než u myši.
+
+◊ls{
+    - k léčení se snažíme využít i kmenové buňky
+    - stárnutí je spojeno s neurodegenerací, vymírají konkrétní populace nervových buněk
+        - např. u Parkinsonovy choroby to jsou dopaminergní neurony v ◊em{corpora nigra}
+    - existují snahy diferencovat určité populace nervových buněk in vitro
+    - regenerace je ale omezenejší než u běžných epitelů
+        - nejsilnější je regenerace v bulbus olfactorius (čichovém bulbu) a v hippokampu, který je plastický i v dospělosti
+}
+
+◊ls{
+    - toto se dá pozorovat i u pacientů trpícími vážnými, život ohrožujícími epileptickými záchvaty
+        - odstraní se velká část mozku s epileptickým ložiskem
+        - původní práci této části zastane druhá hemisféra
+    - Phineas Gage
+        - hlavou mu proletěla tyč
+        - obnovila se mu skvěle řeč i hybnost
+}
+
+◊ls{
+    - víme, kde v myším mozku sídlí kmenové buňky
+    - když je zeleně fluorescenčně označíme a přeneseme do neznačeného mozku, během života myši poté sledujeme, jak se značené buňky všude rozšiřují a jak se obnovují různé neuronální populace
+}
+
+◊definitions{
+    ◊term["mikrochimérismus"]{
+        Buňky myšátek během gravidity osidlují tělo matky, což se dá pozorovat na myšátkách GFP-tagovaného samce a netagované samice.
+    }
+}
+
+◊ls{
+    - hematoencefalická bariéra odděluje mozek od zbytku těla a je běžně pro buňky neprůchodná
+    - v mozku myši indukováno poškozeni
+        - přes hematoencefalickou bariéru masivně pronikají kmenové buňky neznámého původu
+        - zajištění regenerace poměrně velké části nervové tkáně
+        - diferenciace v nervové buňky i různé typy gliových buněk
+}
+
+◊ls{
+    - různé neurální populace se aktivují při různých úkolech
+        - např. při rozlišování hranatých a kulatých věcí
+    - dynamická struktura, která se "drátuje" v průběhu života
+}
+
+◊ls{
+    - skenovací metody často pracují s izotopy prvků, které mají liché počty neutronů
+        - možnost vizualizovat pomocí funkční magnetické rezonance (FMR)
+        - dá se zjistit, které oblasti mozku jsou aktivní a neaktivní
+        - mozek je možno pozorovat in vivo, např. i to, jak reaguje na konkrétní vzruchy
+        - PET (pozitronová emisní tomografie): vychytávání cukru označeného radioaktivní látkou aktivním rostoucím nádorem
+    - mozek je rozdělen na malé specializované části
+        - je těžko pochopitelné, že je poté možno velkou část mozku nahradit druhou hemisférou
+}
+
+◊ls{
+    - součástí správného rozvoje CNS je i apoptóza
+    - neurony, které nejsou za prvních pár týdnů prenatálního života použity, podléhají buněčné smrti
+    - místům v mozku, která byla původně určena jako nefunkční či prázdná, bývá pomocí FMR přiřazena funkce
+    - podobně jsou i v rámci postnatálního života posilovány spoje, které jsou často používány
+        - naopak nepoužívané spoje slábnou a zanikají
+        - je důležité dávat dítěti (alespoň do sedmi let života) co nejvíce různých vjemů
+        - příkladem může být absolutní hudební sluch, který silně souvisí s typem vjemů, kterým je dítě vystavováno
+            - v Asii desetkrát vyšší incidence absolutního sluchu než u nás, snad kvůli tonálním jazykům
+            - je s ním spijený jen jeden gen, který avšak způsobuje i nízkou hodnotu IQ
+    - tato plasticita mozku během života zaniká
+        - netvoří se nové spoje, pouze se posilují a zeslabují ty stávající
+}
+
+◊ls{
+    - v ontogenezi se tvoří obrovské množství buněk
+    - některé projdou programovanou buněčnou smrtí
+    - nezralé neurony během ontogeneze putují podél radiálních gliobých buněk propojujících vnitřní  a vnější povrch nervové trubice (délka až 2cm)
+    - gliové buňky slouží jako pravítko a určují tloušťku vrstev nervových buněk v mozku
+        - tvorba vrstev (viz obrázek níže)
+            # první neuroblasty lezou a vytvoří vrstvu, která se stabilizuje tvorbou mezibuněčných spojení
+            # lezou nové buňky, prolezou stávající vrstvu a vytvoří další vrstvu
+            # neokortex je poslední vrstva přidaných buněk, nejdál od zdroje kmenových buněk
+    - nervové výběžky jsou poté naváděny pomocí chemoatraktantů (např. netrin) a chemorepelentů (např. některé semaforiny, proteiny Slit)
+        - přesná diferenciace v konkrétní populaci je dána poziční informací od hormonů
+            - rodiny Hox, Pax, Dbx, Irx
+            - faktory sonic hedgehog, BMP
+        - někdy se jeden výběžek plazí po druhém, který by pak byl tzv. ◊em{pioneer neuron}
+    - pro přežívá neuronů jsou nutné neurotropiny, např. NGF (nerve growth factor)
+}
+
+◊img["vrstvy_rust.png"]{názornění postupného růstu vrstev podle gliových buněk}
+
+◊section{Senzorické epitely}
+
+◊ls{
+    - buňky na pomezí epitelu a nervové buňky
+        - historicky je od ektodermu odvozena celá nervová soustava i senzorické tkáně čichové, zrakové i sluchové
+    - mají apikální (detekční) a bazální (synaptický) konec
+}
+
+◊subsection{Čichový epitel}
+
+◊img["nos.png"]{Schéma čichového epitelu}
+
+◊ls{
+    - jako jedna z mála neurosenzorických struktur se během života mění
+    - zde se skupina buněk se diferencuje v čichové (viz obrázek výše)
+        - cilie jsou nepohyblivé, obsahují čichové receptory
+        - na bazální straně jeden axon směřující do mozku
+        - obklopeny podpůrnými buňkami s podobným významem jako gliové buňky
+    - senzorické neurony přeživájí 1--2 měsíce
+        - poté jsou nahrazeny diferenciací bazálních buněk
+    - každý senzorický neuron exprimuje jen jeden z několika set čichových receptorů
+    - když jsou buňky obnovovány, nově vznikající buňka si náhodně vybere jeden receptor
+}
+
+◊ls{
+    - axony senzorických neuronů se stejným receptorem jsou rozptýleny v čichové sliznici
+    - axony směřují do stejného glomerulu
+        - u myší je v bulbus olfactorius na každé straně 1800 různých glomerolů
+        - čím více glomerulů, tím více vůní umíme rozeznat, ale i tím více senzorických neuronů musíme mít
+    - jak axony nově vznikajících buněk najdou správnou cestu ke glomerulu
+        - zdá se, že v tom hrají roli receptory pro čich spřažené s G-proteiny
+        - tyto receptory jsou schopny homeotické adheze, tj. dva stejné receptory se "zazipují", ale jiné ne
+        - axon putuje po glomerulech, zůstane tam, kde se váže nejsilněji
+    - existuje mnoho poruch této axonové navigace, lidé ztrácí schopnost kontinuity pachů
+}
+
+Studium navigace axonů se opět provádělo na zelených myškách; zeleně se obarvily jen neurony reagující na jednu konkrétní vůni. Po histologii mozku se ukázalo, že všechny zelené axony míří pouze do dvou míst (glomerulů), jednom na každé hemisféře.
+
+◊subsection{Sluchový epitel}
+
+◊ls{
+    - morfologicky nejpropracovanější tkáň v těle
+    - hlemýžďová rezonanční struktura vzniká prenatálně
+    - záleží na tom, v kterém místě hlemýždě dochází k rezonanci s membránami, které obalují prostory vyplněné tekutinou
+        - voda je nestlačitelná => přenáší vibrace
+    - senzorickými buňkami jsou sluchové vláskové buňky
+}
+
+◊ls[#:t "Vláskové buňky"]{
+    - leží ve struktuře ◊strong{Cortiho orgánu}, mezi podpůrnými buňkami, překryty extracelulární matrix
+    - převádějí mechanickou deformaci v elektrický signál
+    - všechny mají stejnou morfologii varhanovitých výběžků, ◊em{stereocilií}
+        - stabilizovány aktinovým cytoskeletem
+        - podobně jako výběžky na buňkách ve střevě
+        - rozměry každé stereocilie pevně dány vzhledem k poloze ve středním uchu, odpovídají frekvencii zvukového podnětu, na který mají reagovat
+    - neregenerují
+    - propojené přes gap junctions ◊em{konexinem 26}
+}
+
+◊ls[#:t "Princip funkce"]{
+    # zvukové vibrace deformují stereocilia
+    # otevírají se iontové kanály s mechanickými "vrátky"
+        - reálně dochází ke změně konformace iontového kanálu
+    # vzniká membránový vzruch
+    # na bazálním konci dojde v synapsi s neuronem k vylití neurotransmiteru
+}
+
+◊ls[#:t "Choroby"]{
+    - sluch se mění, hlavně ve stáří a hlavně mužům (špatný sluch zvláště ve vyšších frekvencích)
+    - celá řada poruch je genetického původu
+        - mutace v konexinu 26 způsobují hluchotu (jedna z nejčastějších genetických chorob v Evropě)
+}
+
+◊subsection{Zrakový epitel}
+
+◊ls{
+    - fotoreceptory se dělí na tyčinky a čípky
+    - senzorickou složkou jsou proteiny opsiny (◊em{ópsis} = zrak) s prostetickou skupinou ◊strong{retinalem}
+        - retinal je schopný cis-trans izomerizace, když pohltí foton
+        - změna konformace retinalu změní tvar opsinu
+    - není schopný regenerace
+}
+
+◊todo{Doplnit schéma oka.}
+
+Nejblíže u pigmentovaných epiteliálních buněk je senzorický epitel, poté jsou různé interneurony a gangliové neurony, které vysílají signál do mozku. Apikální vrstvu senzorické složy tvoří brva (či přetvořený bičík).
+
+◊ls[#:t "Princip funkce v rámci buňky"]{
+    # retinal změní konformaci
+    # opsin změní tvar
+    # aktivují se cGMP fosfodiesterázy, které štěpí cGMP
+        - v očních buňkách je jinak vysoká koncentrace cGMP
+    # otevřou se ◊chem{Ca^{2+}} kanály, dojde k hyperpolarizaci membrány
+    # uzavřou se ◊chem{Na+} kanály
+    # zastaví se bazální signalizace
+}
+
+To, jakým způsobem vidíme, je vlastně negativ: při zachycení fotonu se zastaví/sníží bazální signalizace. To umožňuje rozlišovat jemnější nuance v signálech.
+
+◊ls[#:t "Rodopsiny"]{
+    - součástí rodiny opsinů, v tyčinkách
+    - superpozicí tří různý rodopsinů vzniká konkrétní barva
+    - mutace v jednom rodopsinu zapříčiní to, že člověk nebude schopen rozeznat nějaké barvy od sebe
+        - jeden z rodopsinů je vázaný na chromozom X, takže se daltonismus vyskytuje častěji u mužů
+}
+
+◊ls[#:t "Pigmentované epiteliální buňky"]{
+    - odrážejí a pohlcují světlo, brání osdleskům
+    - fungují jako makrofágy
+        - senzorické buňky se nemohou během života měnit, proto jen vyměňují svůj obsah
+        - odštěpují váčky s denaturovanými proteiny a kovalentně modifikovanými lipidy
+        - tyto váčky uklízejí právě epiteliální buňky
+}
+
+◊ls[#:t "Choroby"]{
+    - výše zmíněná barvoslepost
+    - mutace mitochondriální DNA => ztráta zraku, atrofie očního nervu
+        - např. syndrom LHON
+        - zrakový nerv a funkce senzorického zrakového epitelu je zřejmě jedna z Achillových pat energetického metabolismu
+}
