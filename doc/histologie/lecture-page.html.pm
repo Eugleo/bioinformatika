8e626b0f89c5e323e625d24fec822ef1c5559dae
Evžen Wybitul <wybitul.evzen@gmail.com>
2019-05-19 19:04:41 +0200

    Add a lecture page placeholder

---
+++ doc/histologie/lecture-page.html.pm
@@ -0,0 +1,17 @@
+#lang pollen
+
+◊define-meta[template]{../../lecture-page-template.html}
+◊define-meta[header]{histo}
+◊define-meta[title]{Histologie/Cytologie}
+
+◊title{Histologie/Cytologie}
+
+◊box["Zápisky (v1.0.0, poslední update 19. 5. 2019)"]{
+    K dispozici je odkaz na ◊link["notes.html"]{webovou verzi}.
+}
+
+◊section{Informace o zkoušce}
+
+◊section{Informace o cvičeních}
+
+◊section{Doporučená literatura}
