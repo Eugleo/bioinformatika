49ec09f809bf452278d32086605ae2f971ce1949
Evžen Wybitul <wybitul.evzen@gmail.com>
2019-06-06 02:01:37 +0200

    Add leture info placeholder

---
+++ doc/strukturni-biologie-bunky/lecture-page.html.pm
@@ -0,0 +1,15 @@
+#lang pollen
+
+◊define-meta[template]{../../lecture-page-template.html}
+◊define-meta[header]{struktury}
+◊define-meta[title]{Strukturní biologie buňky}
+
+◊title{Strukturní biologie buňky}
+
+◊box["Zápisky (v1.1.0, poslední update 16. 5. 2019)"]{
+    K dispozici je odkaz na ◊link["notes.html"]{webovou verzi}.
+}
+
+◊section{Informace o zkoušce}
+
+◊todo{Doplnit informace o zkoušce.}
