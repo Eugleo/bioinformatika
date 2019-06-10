961a23fda7857c0cdb25d85437935b748eeb73bc
Evžen Wybitul <wybitul.evzen@gmail.com>
2019-06-10 18:48:24 +0200

    Update PDF styling

--- doc/neproceduralni-programovani/lecture-page.html.pm
+++
@@ -1,9 +0,0 @@
-#lang pollen
-
-◊define-meta[template]{../../lecture-page-template.html}
-◊define-meta[header]{neproc}
-◊define-meta[title]{Neprocedurální programování}
-
-◊title{Neprocedurální programování}
-
-Tato stránka je jen placeholder, časem se zde obejví zápisy a poznámky z neprocedurálka, stejně jako informace o zkoušce a cvičeních.
