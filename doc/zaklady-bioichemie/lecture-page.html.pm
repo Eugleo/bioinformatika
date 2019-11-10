333f330cdf8c518376e454dc2d6e17d3e0a43b53
Evžen Wybitul <wybitul.evzen@gmail.com>
2019-11-10 08:53:04 +0100

    Setup a new page for biochemistry

---
+++ doc/zaklady-bioichemie/lecture-page.html.pm
@@ -0,0 +1,11 @@
+#lang pollen
+
+◊define-meta[template]{../../lecture-page-template.html}
+◊define-meta[header]{biochem}
+◊define-meta[title]{Základy biochemie}
+
+◊title{Základy biochemie}
+
+◊box["Zápisky (v0.0.1)"]{
+    K dispozici je odkaz na ◊link["notes.html"]{webovou verzi}.
+}
